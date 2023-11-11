6c86c4254ecc5c6a89bd3033ef1d10e1158148b6
fforregor <fforregor@ext.falabella.cl>
2023-11-11 15:22:37 -0500

    Se crean los modulos mivie, platform, review con sus servicios y controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { PlatformsModule } from './platforms/platforms.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule, PlatformsModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
