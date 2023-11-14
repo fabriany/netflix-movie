49d04c887f34f0b813ffa7605052dab51089cac4
fforregor <fforregor@ext.falabella.cl>
2023-11-14 07:34:20 -0500

    Refactor - Se ajusta el modulo de peliculas para pertenecer al damain

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesModule } from './movies/movies.module';
+import { MoviesModule } from './domain/movies/movies.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config';
