d4c60d603ce8a955905ad6552fdd03da10663795
fforregor <fforregor@ext.falabella.cl>
2023-11-15 16:58:23 -0500

    Refactor - se modifica el vercel.json

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api/docs', app, document);
   app.enableCors();
   await app.listen(3000);
 }
